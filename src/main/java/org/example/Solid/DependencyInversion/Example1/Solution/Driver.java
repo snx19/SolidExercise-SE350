8819dadc1a9f741aa42b365ed3d6ad3a43b96a51
snx19 <hayato@msnx.local>
2025-05-15 02:01:10 -0500

    Driver creates new racing car

--- src/main/java/org/example/Solid/DependencyInversion/Example1/Solution/Driver.java
+++ src/main/java/org/example/Solid/DependencyInversion/Example1/Solution/Driver.java
@@ -1,4 +1,13 @@
 package org.example.Solid.DependencyInversion.Example1.Solution;
 
+/**
+ * Represents a driver
+ * can use any type of car
+ */
 public class Driver {
+    public static void main(final String[] args) {
+        RacingCar car = new RacingCar(10);
+
+        car.accelerate();
+    }
 }
