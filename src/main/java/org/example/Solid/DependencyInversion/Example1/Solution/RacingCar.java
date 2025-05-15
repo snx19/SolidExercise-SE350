283b754fe7a885bbc21eac87a3b241845ef02dd4
snx19 <hayato@msnx.local>
2025-05-15 02:00:58 -0500

    Class racing car implements interface Car

--- src/main/java/org/example/Solid/DependencyInversion/Example1/Solution/RacingCar.java
+++ src/main/java/org/example/Solid/DependencyInversion/Example1/Solution/RacingCar.java
@@ -1,4 +1,29 @@
 package org.example.Solid.DependencyInversion.Example1.Solution;
 
-public class RacingCar {
+/**
+ * Represents racing car
+ */
+public class RacingCar implements Car{
+    /** is the remaining fuel racing car has*/
+    private int remainingFuel;
+    /** is the power racing car has*/
+    private int power;
+
+    /**
+     * Constructor creates racing car with set fuel
+     * @param fuel the amount of fuel a racing car has
+     */
+    public RacingCar(final int fuel) {
+        this.remainingFuel = fuel;
+    }
+
+    /**
+     * method accelerates car and reduces fuel
+     */
+    @Override
+    public void accelerate() {
+        power++;
+        remainingFuel--;
+    }
+
 }
