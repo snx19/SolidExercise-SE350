c075ab15f05cd31975131cf03978a07ac8fe317a
snx19 <hayato@msnx.local>
2025-05-15 02:00:17 -0500

    Interface for car

--- src/main/java/org/example/Solid/DependencyInversion/Example1/Solution/Car.java
+++ src/main/java/org/example/Solid/DependencyInversion/Example1/Solution/Car.java
@@ -1,4 +1,12 @@
 package org.example.Solid.DependencyInversion.Example1.Solution;
 
+/**
+ * interface for cars
+ */
 public interface Car {
+
+    /**
+     * accelerates the car by reducing fuel
+     */
+    void accelerate();
 }
